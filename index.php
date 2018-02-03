8c6999fa36af4682ac6df1cae39eebc2e24be7ec
Nolan Medina <njmedina614@gmail.com>
2018-02-02 19:43:42 -0500

    added main body for interests profile and fixed personal-info, index

--- index.php
+++ index.php
@@ -34,22 +34,12 @@ $f3->route('POST /profile', function() {
 
     //print_r($_POST);
 
-    //Array ( [first] => 1234 [last] => 1234 [age] => 1234 [male] => on [phone] => 1234 )
-
-    $gender = "?";
+    //Array ( [first] => Testing123 [last] => Testing123 [age] => Testing123 [gender] => male [phone] => Test123 )
 
     $_SESSION['first'] = $_POST['first'];
-    $_SESSION['last'] = $_POST['first'];
-    $_SESSION['age'] = $_POST['first'];
-
-    if(isset($_POST['male'])){
-        $gender = "Male";
-    }
-    if(isset($_POST['female'])){
-        $gender = 'Female';
-    }
-    $_SESSION['gender'] = $gender;
-
+    $_SESSION['last'] = $_POST['last'];
+    $_SESSION['age'] = $_POST['age'];
+    $_SESSION['gender'] = $_POST['gender'];
     $_SESSION['phone'] = $_POST['phone'];
 
     $template = new Template();
@@ -58,7 +48,17 @@ $f3->route('POST /profile', function() {
 }
 );
 
-$f3->route('GET /interests', function() {
+$f3->route('POST /interests', function() {
+
+    //print_r($_POST);
+
+    //Array ( [email] => jnjeoijoiqj3lknas [state] => WA [gender] => male [bio] => fewfwefwefwef )
+
+    $_SESSION['first'] = $_POST['email'];
+    $_SESSION['last'] = $_POST['state'];
+    $_SESSION['age'] = $_POST['gender'];
+    $_SESSION['gender'] = $_POST['bio'];
+
 
     $template = new Template();
     echo $template->render('pages/interests.html');
@@ -67,7 +67,15 @@ $f3->route('GET /interests', function() {
 );
 
 
-$f3->route('GET /summary', function() {
+$f3->route('POST /summary', function() {
+
+
+    //print_r($_POST);
+    //Array ( [indoor] => Array ( [0] => tv [1] => movies [2] => cooking [3] => board games [4] => puzzles [5] => reading [6] => playing cards [7] => video games )
+    // [outdoor] => Array ( [0] => Hiking [1] => Biking [2] => Swimming [3] => collecting [4] => walking [5] => climbing [6] => sports ) )
+
+    $_SESSION['indoor'] = $_POST['indoor'];
+    $_SESSION['outdoor'] = $_POST['outdoor'];
 
     $template = new Template();
     echo $template->render('pages/summary.html');
