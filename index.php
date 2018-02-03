c8998a5bcec96bda358b83dbf62695196f8df3aa
Nolan Medina <njmedina614@gmail.com>
2018-02-02 21:13:07 -0500

    Finished all base pages w/o validation

--- index.php
+++ index.php
@@ -54,10 +54,10 @@ $f3->route('POST /interests', function() {
 
     //Array ( [email] => jnjeoijoiqj3lknas [state] => WA [gender] => male [bio] => fewfwefwefwef )
 
-    $_SESSION['first'] = $_POST['email'];
-    $_SESSION['last'] = $_POST['state'];
-    $_SESSION['age'] = $_POST['gender'];
-    $_SESSION['gender'] = $_POST['bio'];
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['state'] = $_POST['state'];
+    $_SESSION['seeking'] = $_POST['gender'];
+    $_SESSION['bio'] = $_POST['bio'];
 
 
     $template = new Template();
@@ -67,7 +67,7 @@ $f3->route('POST /interests', function() {
 );
 
 
-$f3->route('POST /summary', function() {
+$f3->route('POST /summary', function($f3) {
 
 
     //print_r($_POST);
@@ -76,6 +76,34 @@ $f3->route('POST /summary', function() {
 
     $_SESSION['indoor'] = $_POST['indoor'];
     $_SESSION['outdoor'] = $_POST['outdoor'];
+
+
+
+    $f3->set('first',$_SESSION['first']);
+    $f3->set('last',$_SESSION['last']);
+    $f3->set('age',$_SESSION['age']);
+    $f3->set('gender',$_SESSION['gender']);
+    $f3->set('phone',$_SESSION['phone']);
+
+    $f3->set('email',$_SESSION['email']);
+    $f3->set('state',$_SESSION['state']);
+    $f3->set('seeking',$_SESSION['seeking']);
+    $f3->set('bio',$_SESSION['bio']);
+
+    $f3->set('indoor', array());
+    $f3->set('outdoor',array());
+
+    foreach($_SESSION['indoor'] as $key=>$value)
+    {
+        // and print out the values
+        $f3->push('indoor', $value);
+    }
+
+    foreach($_SESSION['outdoor'] as $key=>$value)
+    {
+        // and print out the values
+        $f3->push('indoor', $value);
+    }
 
     $template = new Template();
     echo $template->render('pages/summary.html');
