11928d28a3092ed39f0dcb9ae4ad8e0cfde1f72f
Nolan Medina <njmedina614@gmail.com>
2018-02-16 19:40:17 -0500

    added premium button and pass user in session to second page

--- index.php
+++ index.php
@@ -40,14 +40,14 @@ $f3->route('GET /', function() {
 
 $f3->route('GET|POST /personal', function($f3, $errors) {
 
-    /*print_r($_POST);*/
 
     if(isset($_POST['submit'])) {
 
+
         $f3->set('first',$_POST['first']);
-        $f3->set('last',$_POST['first']);
-        $f3->set('ageStick',$_POST['first']);
-        $f3->set('phoneStick',$_POST['first']);
+        $f3->set('last',$_POST['last']);
+        $f3->set('ageStick',$_POST['age']);
+        $f3->set('phoneStick',$_POST['phone']);
         if($_POST['gender']=='male'){
             $f3->set('checkedMale',$_POST['gender']);
         } elseif($_POST['gender']=='female'){
@@ -79,6 +79,18 @@ $f3->route('GET|POST /personal', function($f3, $errors) {
         if (!in_array('name', $errors) && !in_array('age', $errors)
             && !in_array('phone', $errors)) {
 
+            if(isset($_POST['premium'])) {
+                $_SESSION['premium'] = $_POST['premium'];
+                $user = new PremiumMember($_SESSION['first'], $_SESSION['last'], $_SESSION['age'], $_SESSION['gender'], $_SESSION['phone']);
+            } else{
+                $user = new Member($_SESSION['first'], $_SESSION['last'], $_SESSION['age'], $_SESSION['gender'], $_SESSION['phone']);
+            }
+
+            $user1 = serialize($user);
+            $fp = fopen("store", "w");
+            fwrite($fp, $user1);
+            fclose($fp);
+
             $f3->reroute('/profile');
 
         }
@@ -94,6 +106,7 @@ $f3->route('GET|POST /personal', function($f3, $errors) {
         }
 
     }
+
     $template = new Template();
     echo $template->render('pages/personal-info.html');
 
@@ -103,11 +116,9 @@ $f3->route('GET|POST /personal', function($f3, $errors) {
 $f3->route('GET|POST /profile', function($f3)  {
 
     if(isset($_POST['submit'])){
-        $_SESSION['email'] = $_POST['email'];
-        $_SESSION['state'] = $_POST['state'];
-        $_SESSION['seeking'] = $_POST['gender'];
-        $_SESSION['bio'] = $_POST['bio'];
-        $f3->reroute('/interests');
+
+
+
     }
 
 
@@ -138,6 +149,8 @@ $f3->route('GET|POST /interests', function($f3,  $errors) {
                 array_push($errors, 'indoor');
                 break;
             }
+
+
         }
 
         foreach($_SESSION['outdoor'] as $key=>$value)
@@ -150,7 +163,7 @@ $f3->route('GET|POST /interests', function($f3,  $errors) {
 
         if (!in_array('indoor', $errors) && !in_array('outdoor', $errors)) {
 
-            $f3->reroute('/summary');
+
 
         }
 
@@ -175,43 +188,6 @@ $f3->route('GET|POST /interests', function($f3,  $errors) {
 
 $f3->route('GET|POST /summary', function($f3) {
 
-    if(isset($_SESSION['indoor'])){
-        echo "it is set";
-    }
-
-    //print_r($_POST);
-    //Array ( [indoor] => Array ( [0] => tv [1] => movies [2] => cooking [3] => board games [4] => puzzles [5] => reading [6] => playing cards [7] => video games )
-    // [outdoor] => Array ( [0] => Hiking [1] => Biking [2] => Swimming [3] => collecting [4] => walking [5] => climbing [6] => sports ) )
-
-    $f3->set('first',$_SESSION['first']);
-    $f3->set('last',$_SESSION['last']);
-    $f3->set('age',$_SESSION['age']);
-    $f3->set('gender',$_SESSION['gender']);
-    $f3->set('phone',$_SESSION['phone']);
-
-    $f3->set('email',$_SESSION['email']);
-    $f3->set('state',$_SESSION['state']);
-    $f3->set('seeking',$_SESSION['seeking']);
-    $f3->set('bio',$_SESSION['bio']);
-
-    $f3->set('indoor', $_SESSION['indoor']);
-    $f3->set('outdoor',$_SESSION['outdoor']);
-
-    //I encountered an Error Here and couldn't figure out a solution. If you see what is wrong please
-    //send me a message. I think it has something to do with the session not getting stored but i don't
-    //know why that would be.
-
-       /*foreach($_SESSION['indoor'] as $key=>$value)
-       {
-               $f3->push('indoor', $value);
-        }
-
-
-       foreach($_SESSION['outdoor'] as $key=>$value)
-       {
-           // and print out the values
-           $f3->push('indoor', $value);
-       }*/
 
     $template = new Template();
     echo $template->render('pages/summary.html');
